b4c4f276a78ec892ff2eba9919a1624615064694
Ninh Nguyen <ninh.nguyen@arent3d.com>
2025-04-15 16:30:29 +0700

    Restructure folder

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1280,
     viewportHeight: 800,
+    specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
